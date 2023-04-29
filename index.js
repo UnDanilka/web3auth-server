fc31da5afe84ff773019d43cdd88599aacc60090
Danil Elfimov <dancingdanil98@gmail.com>
2023-04-29 15:16:36 +0300

    changed logic

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json({ type: 'application/json' }))
 
-app.get('/emails', async (req, res) => {
-    const emails = await fse.readFileSync(`database.json`)
-    res.send(emails)
+app.get('/wallets', async (req, res) => {
+    const wallets = await fse.readFileSync(`database.json`)
+    res.send(wallets)
 })
 
 app.post('/add', async (req, res) => {
-    const email = req.body
-    const emails = await fse.readFileSync(`database.json`)
-    const parsedEmails = JSON.parse(emails)
-    parsedEmails.push(email)
-    await fse.outputFile(`database.json`, JSON.stringify(parsedEmails));
-    res.json(parsedEmails)
+    const wallet = req.body
+    const wallets = await fse.readFileSync(`database.json`)
+    const parsedWallets = JSON.parse(wallets)
+    parsedWallets.push(wallet)
+    await fse.outputFile(`database.json`, JSON.stringify(parsedWallets));
+    res.json(parsedWallets)
 })
 
 app.listen(4000, () => {
